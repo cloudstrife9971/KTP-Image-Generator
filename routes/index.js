e442f7d07321a844019e6781bf6fa2dfd7b08c4c
cloudstrife9971 <45213804+cloudstrife9971@users.noreply.github.com>
2022-10-26 03:11:05 +0530

    queries added

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,87 @@ const puppeteer = require("puppeteer");
 
 router.get("/", function (req, res, next) {
   (async () => {
+    var nik = req.query.nik;
     var name = req.query.name;
     var region = req.query.region;
-    var data = req.params.data;
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    var dob = req.query.dob;
+    var gender = req.query.gender;
+    var bloodType = req.query.bloodType;
+    var address = req.query.address;
+    var town = req.query.town;
+    var district = req.query.district;
+    var religion = req.query.religion;
+    var maritalStatus = req.query.maritalStatus;
+    var occupation = req.query.occupation;
+    var citiizenship = req.query.citiizenship;
+    var idValidity = req.query.idValidity;
+    var region2 = req.query.region2;
+    var issuanceIdDdate = req.query.issuanceIdDdate;
+
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     const page = await browser.newPage();
     await page.goto(
-      "http://localhost:5000/users/?" +
+      // "http://localhost:"+process.env.PORT +"/users/?" +
+      //  "http://localhost:3000/users/?" +
+      //     "name=" +
+      //     name +
+      //     "&" +
+      //     "region=" +
+      //     region
+      "http://localhost:3000/users/?" +
+        "nik=" +
+        nik +
+        "&" +
         "name=" +
         name +
         "&" +
         "region=" +
-        region
+        region +
+        "&" +
+        "dob=" +
+        dob +
+        "&" +
+        "gender=" +
+        gender +
+        "&" +
+        "bloodType=" +
+        bloodType +
+        "&" +
+        "address=" +
+        address +
+        "&" +
+        "town=" +
+        town +
+        "&" +
+        "district=" +
+        district +
+        "&" +
+        "religion=" +
+        religion +
+        "&" +
+        "maritalStatus=" +
+        maritalStatus +
+        "&" +
+        "occupation=" +
+        occupation +
+        "&" +
+        "citiizenship=" +
+        citiizenship +
+        "&" +
+        "idValidity=" +
+        idValidity +
+        "&" +
+        "region2=" +
+        region2 +
+        "&" +
+        "issuanceIdDdate=" +
+        issuanceIdDdate
     );
     await page.screenshot({ path: "KTP.jpeg" });
     await browser.close();
     res.download("KTP.jpeg");
     // res.send("Welcome to KTP image Downloader")
-    
   })();
- 
-  
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,24 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.render("index",{ name: req.query.name, region: req.query.region });
+  res.render("index", {
+    nik: req.query.nik,
+    name: req.query.name,
+    region: req.query.region,
+    dob: req.query.dob,
+    gender: req.query.gender,
+    bloodType: req.query.bloodType,
+    address: req.query.address,
+    town: req.query.town,
+    district: req.query.district,
+    religion: req.query.religion,
+    maritalStatus: req.query.maritalStatus,
+    occupation: req.query.occupation,
+    citiizenship: req.query.citiizenship,
+    idValidity: req.query.idValidity,
+    region2: req.query.region2,
+    issuanceIdDdate: req.query.issuanceIdDdate
+  });
 });
-
 
 module.exports = router;
